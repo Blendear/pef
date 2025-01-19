fd841d46344e0e15db0e6b86784c09a8a56e4de0
Blendear <tobiascibis@gmail.com>
2025-01-19 21:37:01 +0100

    fix: Update types

--- src/features/a_template lean/components/AClassicComponent.tsx
+++ src/features/a_template lean/components/AClassicComponent.tsx
@@ -3,13 +3,11 @@ import { css } from "@emotion/react";
 import { variables } from "@/styles/emotion-css/variables";
 import { universalCss } from "@/styles/emotion-css/universal";
 import { colors } from "@/styles/emotion-css/colors";
-import { OneTwoThreeFourWordComponentNameProps } from "@/types";
 
 const oneTwoTFWCNCss = {
   container: css({}),
 };
 
-export const OneTwoThreeFourWordComponentName =
-  ({}: OneTwoThreeFourWordComponentNameProps) => {
-    return <div>OneTwoThreeFourWordComponentName</div>;
-  };
+export const OneTwoThreeFourWordComponentName = ({}) => {
+  return <div>OneTwoThreeFourWordComponentName</div>;
+};
